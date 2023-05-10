5388f4d8d70fcc3ddf865bb935ef2109fc933a73
justinwb <justinwb@users.noreply.github.com>
2023-05-10 00:24:13 +0000

    deploy: 8d81fdfbb5f2760fbc544b7911cb329604954d77

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.janeirodigital.sai.httputils"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.janeirodigital.sai.httputils"}];updateSearchResults();
